58e80e05f686676273626aff708e8900654d6a95
Adam Codega <acodega@users.noreply.github.com>
2025-01-31 13:25:59 -0500

    Update computerNameFromInput.sh

--- computerNameFromInput.sh
+++ computerNameFromInput.sh
@@ -5,10 +5,10 @@ title="Name This Mac"
 message="Enter the preferred computer name below"
 
 hwType=$(/usr/sbin/system_profiler SPHardwareDataType | grep "Model Identifier" | grep "Book")  
-if [ "$hwType" != "" ]; then	
-  icon="SF=laptopcomputer"	
-  else	
-  icon="SF=desktopcomputer"	
+if [ "$hwType" != "" ]; then
+  icon="SF=laptopcomputer"
+  else
+  icon="SF=desktopcomputer"
 fi
 
 dialogCMD="$dialogApp -p --title \"$title\" \
